761760f8dc9812d7bc772f30735f71779cc45528
mister-nothing00 <francescodavidedivita@gmail.com>
2025-02-19 21:24:45 +0100

    Project optimizzed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 //MIddlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://portfolio-6q18.onrender.com",
     methods: ["POST", "GET"],
   })
 );
